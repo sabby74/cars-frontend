08a899a132962b2413a57f1a24a14b7fd0bf958c
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-09-27 17:55:42 -0400

    added new files and tailwind installed & axios ins

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Login from './components/Login';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1 className=" bg-indigo-400 h-16 text-3xl font-bold flex items-center  ">
+      Car Mantainance App
+    </h1>
+    <Header />  
+    <Login />
+    <Outlet />
+
+   
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,32 @@
+// Login.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('https://carsapp-pgj8.onrender.com/user', {
+        username,
+        password,
+      });
+
+      // Handle the token received in the response (store it, set authentication state, etc.)
+      console.log(response.data.token);
+    } catch (error) {
+      console.error('Login failed', error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
