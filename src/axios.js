059502cc160c3f660b7f6fc233e3944c192e501c
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-09-27 18:00:41 -0400

    created a mew file axios.js

---
+++ src/axios.js
@@ -0,0 +1,17 @@
+// axios.js (or in your main index.js file)
+import axios from 'axios';
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
