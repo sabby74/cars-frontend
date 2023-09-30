a2fbad3de927ef68094c0f2ef9d0b331eed80c73
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-09-30 17:25:10 -0400

    changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,64 @@
 // Login.js
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('https://carsapp-pgj8.onrender.com/user/login', {
-        username,
-        password,
-      });
+      const response = await axios.post(
+        "http://localhost:7070/user/login",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       // Handle the token received in the response (store it, set authentication state, etc.)
-      
+
       console.log(response.data);
-      if (response.data == true ) {
+      if (response.data == true) {
         navigate('/');
-        // console.log(await axios.get('http://localhost:7070/test' ,{ withCredentials: true }))
-      }else{
-        alert("Wrong Username or Password")
+        // await axios.get("https://carsapp-pgj8.onrender.com/test", {
+        //   withCredentials: true,
+        // });
+      } else {
+        alert("Wrong Username or Password");
       }
     } catch (error) {
-      console.error('Login failed', error);
+      console.error("Login failed", error);
     }
   };
 
   return (
-    <div className='bg-green-700 flex flex-row justify-around p-10 m-6 rounded-lg text-slate-700 t '>
-      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <button className="rounded bg-green-400 p-2 hover:italic" onClick={handleLogin}>Login</button>
+    <div className="bg-green-700 flex flex-row justify-around p-10 m-6 rounded-lg text-slate-700 t ">
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        className="rounded bg-green-400 p-2 hover:italic"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
     </div>
   );
 };
--- src/loaders.js
+++ src/loaders.js
@@ -1,28 +1,36 @@
-const URL = 'https://carsapp-pgj8.onrender.com';
+const URL = 'http://localhost:7070';
 
 
 export const ServiceLoader = async () => {
-  const res = await fetch(URL + "/service")
+  const res = await fetch(URL + "/service", {
+    credentials: "include",
+  } )
   const services = await res.json()
   console.log(services);
   return services
 }
 export const ShowLoader = async ({ params }) => {
-  console.log(params, "ShowLoader");
-  const res = await fetch(URL + "/service/" + params.id);
+  console.log(params, "ShowLoader",);
+  const res = await fetch(URL + "/service/" + params.id, {
+    credentials: "include",
+  });
   const service = await res.json();
   return service;
 };
 
 
   export const UserLoader = async () => {
-    const res = await fetch(URL + "/user")
+    const res = await fetch(URL + "/user",{
+      credentials: "include",
+    })
     const users = await res.json()
     return users
   }
 
 export const SignUpLoader = async () => {
-    const res = await fetch(URL + "/user/signup")
+    const res = await fetch(URL + "/user/signup", {
+      credentials: "include",
+    })
     const signups = await res.json()
     return signups
 }
