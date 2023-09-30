0db5280beedb94fcfb0b5a63d93ddd9f826d228f
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-09-30 12:07:36 -0400

    changes

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+          <Link className="hover:italic" to="/">
+          <div>Cars Maintainance App Home </div>
+        </Link>
+    </div>
+  )
+}
+
+export default Dashboard
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,30 @@
 // Login.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const navigate = useNavigate();
+
   const handleLogin = async () => {
     try {
-      const response = await axios.post('https://carsapp-pgj8.onrender.com/user/login', {
+      const response = await axios.post('http://localhost:7070/user/login', {
         username,
         password,
       });
 
       // Handle the token received in the response (store it, set authentication state, etc.)
+      
       console.log(response.data);
+      if (response.data == true ) {
+        navigate('/');
+        // console.log(await axios.get('http://localhost:7070/test' ,{ withCredentials: true }))
+      }else{
+        alert("Wrong Username or Password")
+      }
     } catch (error) {
       console.error('Login failed', error);
     }
